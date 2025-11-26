5e6edea9d24a5b151a53173d3bec305717774c3d
vnay-dev <cleancode4dev@gmail.com>
2025-11-26 23:05:56 +0530

    Enhance typography and layout for improved readability and responsiveness
    - Added default heading sizes based on Material Design Type Scale in globals.css for consistent typography across the application.
    - Implemented responsive scaling for headings and paragraph styles in responsive.css to ensure optimal readability on mobile devices.
    - Refactored the Hero component to improve layout and spacing, enhancing the overall user experience with better visual hierarchy and alignment.
    - Updated README.md to reflect changes in typography and component structure.

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -2,10 +2,21 @@ import { Container } from "@/components/shared/composite";
 
 export function Hero() {
   return (
-    <section className="min-h-screen w-full">
+    <section className="w-full">
       <Container>
-        {/* Content wrapper */}
-        <div className="flex min-h-screen flex-col items-center justify-center"></div>
+        <div className="flex flex-col gap-8 pt-16 sm:gap-12 sm:pt-24 md:pt-32">
+          <img
+            src="/images/profile_pic.jpg"
+            alt="Hero"
+            className="h-[120px] w-[120px] rounded-lg object-cover sm:h-[150px] sm:w-[150px] md:h-[180px] md:w-[180px]"
+          />
+          <div className="flex flex-col gap-3 sm:gap-4">
+            <h1 className="display-medium">Designer and engineer</h1>
+            <p className="headline-small">
+              Self taught product designer who can visualize & build software
+            </p>
+          </div>
+        </div>
       </Container>
     </section>
   );
