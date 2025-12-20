fa8c9937a6092677eb7f0ea08084513552ed089d
vnay-dev <cleancode4dev@gmail.com>
2025-12-20 20:14:56 +0530

    added new sections and fixed responsiveness

--- app/projects/whatsapp-audio-summary/page.tsx
+++ app/projects/whatsapp-audio-summary/page.tsx
@@ -54,6 +54,61 @@ export default function WhatsAppAudioSummary() {
               </p>
             </div>
           </div>
+
+          <p className="body-xlarge">
+            Ever been stuck in a situation where someone important sends you a bunch of voice notes
+            but you just can&apos;t listen or reply right away?
+          </p>
+
+          <Image
+            src="/images/whatsapp_case_study_prototype.gif"
+            alt="WhatsApp Case Study Prototype"
+            width={1200}
+            height={800}
+            quality={100}
+            className="h-auto w-full rounded-lg object-cover"
+          />
+
+          <p className="body-xlarge">
+            Imagine you&apos;re in an important meeting and your parents send a bunch of voice
+            notes. You can&apos;t check them and not knowing what they&apos;re about keeps you
+            distracted.
+          </p>
+
+          <Image
+            src="/images/whtsp_claws.gif"
+            alt="WhatsApp Claws Animation"
+            width={1200}
+            height={800}
+            quality={100}
+            className="h-auto w-full rounded-lg object-cover"
+          />
+
+          <div
+            className="flex flex-col items-center justify-between gap-8 rounded-lg px-6 pt-5 sm:px-8 sm:pt-6 md:items-center md:gap-8 md:px-12 md:pt-12 lg:flex-row"
+            style={{ backgroundColor: "#fff5eb" }}
+          >
+            <div className="flex w-full flex-col gap-4">
+              <h2 className="headline-small md:!text-[1.75rem]">
+                Scrolling through a long list of voice notes..
+              </h2>
+              <p className="body-xlarge">
+                ...means playing each one separately. If they&apos;re all over the place or mixed
+                with other messages, it&apos;s tough to make sense of them, especially when the
+                conversation&apos;s old.
+              </p>
+            </div>
+            <div className="flex w-full items-end justify-center md:pt-16">
+              <Image
+                src="/images/whtsp_voice_notes_list.png"
+                alt="WhatsApp Voice Notes List"
+                width={1200}
+                height={800}
+                quality={100}
+                className="h-auto w-4/5 rounded-lg object-contain sm:w-3/4 md:w-7/10"
+              />
+            </div>
+          </div>
         </div>
       </Container>
     </main>
