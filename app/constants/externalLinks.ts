c452ef61f9ca7962fe8a0b4a82e6d1b3868b8b76
vnay-dev <cleancode4dev@gmail.com>
2026-05-22 19:33:51 +0530

    resume link updated

--- app/constants/externalLinks.ts
+++ app/constants/externalLinks.ts
@@ -1,3 +1,2 @@
 /** Public resume (Google Drive). */
-export const RESUME_URL =
-  "https://drive.google.com/file/d/1DuzK80EPioOylW8U77oKnAUy_gsqu0qv/view?usp=drive_link";
+export const RESUME_URL = "https://drive.google.com/file/d/15lwOo10ggobou39gO9PrTne6FnqmiN7U/view?usp=drive_link"
